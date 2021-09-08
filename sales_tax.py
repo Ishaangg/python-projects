949de023ff6c8b4344fc2ea4d0b4554aca161756
Ishaangg <ishaangg12@gmail.com>
2021-09-08 15:49:41 +0530

    comment

--- sales_tax.py
+++ sales_tax.py
@@ -1,6 +1,6 @@
 purchaseAmount = eval(input("Enter purchase amount: ")) # Enter the amount
 
-# Suppose the tax is 6 percent
+# Suppose the tax is 6 percentt
 
 tax = (purchaseAmount * 6) / 100.0 # Compute sales tax
 
