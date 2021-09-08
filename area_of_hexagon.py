da58af343fb6d658cad9e59956fc5099e31d74e2
Ishaangg <ishaangg12@gmail.com>
2021-09-08 14:19:15 +0530

    area of hexagon

---
+++ area_of_hexagon.py
@@ -0,0 +1,7 @@
+length_of_side = eval(input("Enter the side of hexagon:  "))
+
+area = "{:.2f}".format(((3 * (3)**0.5) / 2) * length_of_side **2 ) 
+
+
+
+print(f"Area of hexagon of {length_of_side} side is {area}")
